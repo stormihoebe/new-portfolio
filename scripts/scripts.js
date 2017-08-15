4cb74e3c8a7b27fbc46f172b80c5baffacc3c570
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-14 21:46:31 -0700

    add resume pdf

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,17 @@
-var selectedClass = "about";
+var selectedClass;
 
 $(function() {
+  selectedClass = "education";
 		$(".fil-cat").click(function(){
-		selectedClass = $(this).attr("data-rel");
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300);
+  		selectedClass = $(this).attr("data-rel");
+      $(".menu-item").removeClass("active");
+      $(this).addClass("active");
+      $("#portfolio").fadeTo(100, 0.1);
+  		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+      setTimeout(function() {
+        $("."+selectedClass).fadeIn().addClass('scale-anm');
+        $("#portfolio").fadeTo(300, 1);
+      }, 300);
 
 	});
 });
